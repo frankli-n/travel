0d15eaffa444e2185c597a58874eab500467f895
Frankie <frankiehaubrich@gmail.com>
2023-02-06 12:34:55 +1100

    First commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import airbnbnlogo from '../images/airbnb-logo.png'
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src={airbnbnlogo} className="nav--logo" />
+            hi
+        </nav>
+    )
+}
